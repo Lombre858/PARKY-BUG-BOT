df41e714d88f5c72c489fe5fb0c1a9a32b0c381a
Lombre858 <lombrekiyotaka@gmail.com>
2024-09-22 19:49:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,14 +11,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "22898133388"
-global.ownername = "🩵 𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛"
+global.ownernomer = "243834434907"
+global.ownername = "🩵 ⃢𝐁𝐋𝐀𝐂𝐊 ⃤  ☯ ⃢𝐃𝐑𝐀𝐆𝐎𝐍 ༒"
 global.ytname = "YT: Jean-parker-tech"
 global.socialm = "GitHub: Jeanparker100"
 global.location = "Togo, maritime, Lomé"
 
-global.ownernumber = '22898133388'  //creator number
-global.ownername = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛' //owner name
+global.ownernumber = '243834434907'  //creator number
+global.ownername = '⃢𝐁𝐋𝐀𝐂𝐊 ⃤  ☯ ⃢𝐃𝐑𝐀𝐆𝐎𝐍 ༒' //owner name
 global.botname = '𝙿𝙰𝚁𝙺𝚈-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
 
 //sticker details
